6c648653aae48a32e834b10e0ce02c54d120997e
Colin Hehn <colin45h@yahoo.com>
2020-11-08 14:13:21 -0600

    Initial Commit

---
+++ Mod4_CreativeApp_MadlibsHehnC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mod4_CreativeApp_MadlibsHehnC
+//
+//  Created by Tiger Coder on 11/8/20.
+//  Copyright © 2020 clc.hehn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
